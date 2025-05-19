5df2eafa96815d5a8a01e0b139ae761f3be214e9
Abdul Rosyid <abdulrosyid1001@gmail.com>
2025-05-19 14:23:58 +0700

    change to folder file
    add level design and probability

---
+++ Pet Cafe New Update/scripts/levelMissionData.js
@@ -0,0 +1,270 @@
+export const levelMissionDataArr = 
+[
+    {
+        "level": 1,
+        "goals": [
+            {
+                "goalsID": 6,
+                "qty": 3,
+            }
+        ],
+        "moves": 50,
+        "probabilities" :[0.10, 0.30, 0.00, 0.20, 0.30, 0.10]
+    },
+    {
+        "level": 2,
+        "goals": [
+            {
+                "goalsID": 5,
+                "qty": 2
+            }
+        ],
+        "moves": 50,
+        "probabilities" :[0.44, 0.19, 0.13, 0.00, 0.13, 0.13]
+    },
+    {
+        "level": 3,
+        "goals": [
+            {
+                "goalsID": 7,
+                "qty": 1
+            }
+        ],
+        "moves": 40,
+        "probabilities" :[0.40, 0.30, 0.25, 0.05, 0.00, 0.00]
+    },
+    {
+        "level": 4,
+        "goals": [
+            {
+                "goalsID": 6,
+                "qty": 2,
+            },
+            {
+                "goalsID": 7,
+                "qty": 1,
+            }
+        ],
+        "moves": 50,
+        "probabilities" :[0.44, 0.12, 0.16, 0.16, 0.12, 0.00]
+    },
+    {
+        "level": 5,
+        "goals": [
+            {
+                "goalsID": 7,
+                "qty": 1
+            }
+        ],
+        "moves": 50,
+        "probabilities" :[0.52, 0.14, 0.17, 0.00, 0.10, 0.07]
+    },
+    {
+        "level": 6,
+        "goals": [
+            {
+                "goalsID": 13,
+                "qty": 5
+            }
+        ],
+        "moves": 50,
+        "probabilities" :[0.32, 0.23, 0.18, 0.09, 0.14, 0.05]
+    },
+    {
+        "level": 7,
+        "goals": [
+            {
+                "goalsID": 13,
+                "qty": 6,
+            }
+        ],
+        "moves": 40,
+        "probabilities" :[0.59, 0.18, 0.05, 0.00, 0.18, 0.00]
+    },
+    {
+        "level": 8,
+        "goals": [
+            {
+                "goalsID": 5,
+                "qty": 1
+            },
+            {
+                "goalsID": 6,
+                "qty": 1
+            }
+        ],
+        "moves": 30,
+         "probabilities" :[0.91, 0.00, 0.00, 0.09, 0.00, 0.00]
+    },
+    {
+        "level": 9,
+        "goals": [
+            {
+                "goalsID": 6,
+                "qty": 2
+            },
+            {
+                "goalsID": 7,
+                "qty": 1
+            }
+        ],
+        "moves": 30,
+        "probabilities" :[0.43, 0.23, 0.17, 0.17, 0.00, 0.00]
+    },
+    {
+        "level": 10,
+        "goals": [
+            {
+                "goalsID": 13,
+                "qty": 12,
+            }
+        ],
+        "moves": 30,
+        "probabilities" :[0.82, 0.00, 0.09, 0.00, 0.09, 0.00]
+    },
+    {
+        "level": 11,
+        "goals": [
+            {
+                "goalsID": 8,
+                "qty": 2,
+            }
+        ],
+        "moves": 40,
+        "probabilities" :[0.36, 0.18, 0.00, 0.39, 0.00, 0.07]
+    },
+    {
+        "level": 12,
+        "goals": [
+            {
+                "goalsID":9,
+                "qty": 1,
+            }
+        ],
+        "moves": 50,
+        "probabilities" :[0.33, 0.50, 0.00, 0.11, 0.06, 0.00]
+    },
+    {
+        "level": 13,
+        "goals": [
+            {
+                "goalsID": 6,
+                "qty": 2,
+            }
+        ],
+        "moves": 35,
+        "probabilities" :[0.67, 0.00, 0.08, 0.00, 0.17, 0.08]
+    },
+    {
+        "level": 14,
+        "goals": [
+            {
+                "goalsID": 5,
+                "qty": 3,
+            },
+            {
+                "goalsID": 6,
+                "qty": 2,
+            }
+        ],
+        "moves": 35,
+        "probabilities" :[0.39, 0.25, 0.18, 0.07, 0.11, 0.00]
+    },
+    {
+        "level": 15,
+        "goals": [
+            {
+                "goalsID": 9,
+                "qty": 2,
+            }
+        ],
+        "moves": 25,
+        "probabilities" :[0.37, 0.26, 0.05, 0.11, 0.21, 0.00]
+    },
+    {
+        "level": 16,
+        "goals": [
+            {
+                "goalsID": 13,
+                "qty": 4,
+            }
+        ],
+        "moves": 50,
+        "probabilities" :[0.41, 0.26, 0.26, 0.04, 0.04, 0.00]
+    },
+    {
+        "level": 17,
+        "goals": [
+            {
+                "goalsID": 13,
+                "qty": 6,
+            },
+             {
+                "goalsID": 6,
+                "qty": 1,
+            }
+        ],
+        "moves": 35,
+        "probabilities" :[0.00, 0.00, 0.70, 0.20, 0.10, 0.00]
+    },
+    {
+        "level": 18,
+        "goals": [
+            {
+                "goalsID": 13,
+                "qty": 6,
+            },
+            {
+                "goalsID": 8,
+                "qty": 1,
+            }
+        ],
+        "moves": 35,
+        "probabilities" :[0.33, 0.20, 0.40, 0.07, 0.00, 0.00]
+    },
+    {
+        "level": 19,
+        "goals": [
+            {
+                "goalsID": 5,
+                "qty": 3,
+            }
+        ],
+        "moves": 50,
+        "probabilities" :[0.35, 0.35, 0.15, 0.05, 0.05, 0.05]
+    },
+    {
+        "level": 20,
+        "goals": [
+            {
+                "goalsID": 13,
+                "qty": 10,
+            },
+            {
+                "goalsID": 5,
+                "qty": 1,
+            }
+        ],
+        "moves": 35,
+        "probabilities" :[0.46, 0.00, 0.31, 0.08, 0.15, 0.00]
+    }
+]
+
+//index of GoalsID
+// 0 = Betta Fish
+// 1 = Hamster
+// 2 = Canary
+// 3 = Hedgehog
+// 4 = Sugar Glider
+// 5 = Owl
+// 6 = Ferret
+// 7 = Rabbit
+// 8 = Racoon
+// 9 = Cat
+// 10 = Dog
+// 11 = Alpaca
+// 12 = Albino Lion
+// 13 = Wood Box
+// 14 = Jam Box
+// 15 = Ice Box
+// 16 = Mystery Box
