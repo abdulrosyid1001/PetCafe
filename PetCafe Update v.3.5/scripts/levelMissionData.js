b53b48355e3f9f17d3dc7e86052ae491564fecb4
Angel <catalystra@gmail.com>
2025-09-15 14:35:10 +0700

    Level Design Update
    Object Repository: Level 1-2 Layout
    LevelMissionData: v1.8

--- PetCafe Update v.3.5/scripts/levelMissionData.js
+++ PetCafe Update v.3.5/scripts/levelMissionData.js
@@ -1,4 +1,4 @@
-//v1.6
+//v1.8
 export const levelMissionDataArr = 
 [
     {
@@ -6,26 +6,26 @@ export const levelMissionDataArr =
         "goals": [
             {
                 "goalsID": 5,
-                "qty": 3
+                "qty": 2
             }
         ],
         "moves": 50,
          "isprobabilites" : false,
         "probabilities" :[0.40, 0.40, 0.10, 0.10, 0.00, 0.00],
-        "nexttileseq" : [2,2,3,2,1,1,2,3,1,3,1,2,1,0,2,4,1,0,2,1,3,2,0,4,0,2,2,1,2,2]
+        "nexttileseq" : [1,0,1,2,2,3,1,0,1,0,3,4,1,0,1,2,3,4,1,0,1,2,3,4,1,0,1,2,3,4]
     },
     {
         "level": 2,
         "goals": [
             {
                 "goalsID": 6,
-                "qty": 2
+                "qty": 3
             }
         ],
         "moves": 50,
         "isprobabilites" : false,
         "probabilities" :[0.20, 0.20, 0.20, 0.00, 0.40, 0.00],
-        "nexttileseq" : [2,1,1,0,2,4,2,1,0,1,4,5,0,1,2,3,4,5,0,1,2,3,4,5,0,1,2,3,4,5]
+        "nexttileseq" : [3,3,4,3,2,2,3,4,2,4,2,3,2,1,3,5,2,1,3,2,4,3,1,5,1,3,3,2,3,3]
     },
     {
         "level": 3,
